d05fdfcdf9aa0d65dad2c6dc3f2b8f488dec924d
Bogdan Jude <43954492+kogaion28@users.noreply.github.com>
2019-10-03 15:15:46 -0700

    actualizare

---
+++ php/contact.php
@@ -0,0 +1,98 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>MedicSoft | Contacte</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+					<a href="" style="font-size: 30px;">MedicSoft </a>
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.html">Acasa</a></li>
+					
+						<li class="active"><a href="contact.php">contact</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">				
+				<div class="col span_1_of_3">
+					
+      			<div class="company_address">
+				     	<h2>Adresa spitalului :</h2>
+						    	<p>Str.Ionescului nr.21 </p>
+						   		<p>Judetul Alba Orasul Alba Iulia</p>
+						   		<p>Romania</p>
+				   		<p>Telefon:(+40) 723124121</p>
+				   		<p>FixȘ 0258 21312314</p>
+				 	 	<p>Email: <span>MedicSoft@exemplu.com</span></p>
+				   	
+				   </div>
+				</div>				
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contacteazane </h2>
+					    <form>
+					    	<div>
+						    	<span><label>Nume si Prenume</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-mail</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>Nr.Telefon</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>Subiect</label></span>
+						    	<span><textarea> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="Trimite"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>				
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      <div class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-left">
+		   			<ul>
+						<li><a href="index.html">Acasa</a></li>
+						
+						<li><a href="contact.php">Contacte</a></li>
+					</ul>
+		   	</div>
+		  
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+	
+	</body>
+</html>
+
